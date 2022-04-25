e7ac676ee326fa0e8c6199132e8feff848e121bf
Natanael Bezerra <nsbbezerra@hotmail.com>
2022-04-25 08:30:40 -0300

    change route

--- configs/index.ts
+++ configs/index.ts
@@ -3,7 +3,7 @@ const configs = {
     "pk_test_51KVhUEE6fYYQIxKBtgTIUDjGvMso5hDOi8rRiWbvzWG6nhro8mVyud16xeDZ9jd4hhDbFmVfXg9m5Vk8PTaYqU2D00UbHyzKKH",
   stripe_secret_key:
     "sk_test_51KVhUEE6fYYQIxKBCbdgdimLCRPATakWgJD8yQdVHK9nWdVYJJcy0jbWpvvSPwevX4NoYxfvVgYL4FmUpkq4NPtG00AlQbfTyY",
-  api_url: "http://localhost:4003",
+  api_url: "https://nk-curso.herokuapp.com",
   discord: "https://discord.gg/H2rwTF2U",
   whatsapp: "63999711716",
 };
