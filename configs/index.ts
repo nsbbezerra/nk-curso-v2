69487c775baccfed9c98b2040e42fc1b48674c62
Natanael Bezerra <nsbbezerra@hotmail.com>
2022-04-25 09:41:45 -0300

    change whatsapp number

--- configs/index.ts
+++ configs/index.ts
@@ -1,7 +1,7 @@
 const configs = {
   api_url: "https://nk-curso.herokuapp.com",
   discord: "https://discord.gg/H2rwTF2U",
-  whatsapp: "63999711716",
+  whatsapp: "+5563999711716",
 };
 
 export { configs };
